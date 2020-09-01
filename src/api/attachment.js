34608c1d2f5f7de6584688d0d7700c8c99473965
rajiv <rajiv.furtado@platum.io>
2020-09-01 17:56:20 -0100

    fix api attachment

--- src/api/attachment.js
+++ src/api/attachment.js
@@ -21,44 +21,32 @@ export default function Attachment(opts) {
 function AttachmentApi(conf) {
 
     return Object.freeze({
-        get: get,
-        getById: getById,
         create: create,
         update: update,
-        destroy: destroy,
-        uploadFile: uploadFile
+        destroy: destroy
     });
 
-    async function get(query) {
-        const requestParams = query && getQueryString(query);
-        const requestUrl = requestParams
-            ? `${conf.attachmentURL}?${requestParams}`
-            : `${conf.attachmentURL}`;
-        const response = await fetch(requestUrl, {
-            method: "GET",
-            headers: getRequestHeader(conf.apiKey, conf.apiKeyHeader)
-        });
-        return response;
-    }
-
-    async function getById(id) {
-        if (id) {
-            const response = await fetch(`${conf.attachmentURL}/${id}`, {
-                method: "GET",
-                headers: getRequestHeader(conf.apiKey, conf.apiKeyHeader)
+    async function create(file, formData) {
+        if(file) {
+            const fileData = await fetch(`${conf.attachmentUrl}`, {
+                method: "POST",
+                headers: getRequestHeader(conf.apiKey, conf.apiKeyHeader),
+                body: {
+                    name: file.name,
+                    file_type: file.type,
+                    workspace_id: conf.workspaceId
+                }
+            });
+            formData = formData || new FormData();
+            formData.append("file", file);
+            const response = await fetch(`${conf.attachmentUrl}/${fileData.id}/upload`, {
+                method: "POST",
+                headers: getUploadRequestHeader(conf.apiKey, conf.apiKeyHeader),
+                body: formData
             });
             return response;
         }
-        return null;
-    }
-
-    async function create(attachment) {
-        const response = await fetch(`${conf.attachmentURL}`, {
-            method: "POST",
-            headers: getRequestHeader(conf.apiKey, conf.apiKeyHeader),
-            body: attachment
-        });
-        return response;
+        throw new Error("File not provided!");    
     }
 
     async function update(attachment) {
@@ -84,52 +72,22 @@ function AttachmentApi(conf) {
         throw new Error("Attachment Id not provided!");
     }
 
-    async function uploadFile(file, formData) {
-        if(file) {
-            const fileData = await fetch(`${conf.attachmentUrl}`, {
-                method: "POST",
-                headers: getRequestHeader(conf.apiKey, conf.apiKeyHeader),
-                body: {
-                    name: file.name,
-                    file_type: file.type,
-                    workspace_id: conf.workspaceId
-                }
-            });
-            formData = formData || new FormData();
-            formData.append("file", file);
-            const response = await fetch(`${conf.attachmentUrl}/${fileData.id}/upload`, {
-                method: "POST",
-                headers: getUploadRequestHeader(conf.apiKey, conf.apiKeyHeader),
-                body: formData
-            });
-            return response;
-        }
-        throw new Error("File not provided!");    
-    }
-
 }
 
 function AttachmentLocal(conf = {}) {
 
     return Object.freeze({
-        get: get,
-        getById: getById,
         create: create,
         update: update,
-        destroy: destroy,
-        uploadFile: uploadFile
+        destroy: destroy
     });
 
-    async function get(query) {
-        return await localFetch("attachment.get", { query }, conf.apiVersion);
-    }
-
-    async function getById(id) {
-        return await localFetch("attachment.getById", { id }, conf.apiVersion);
-    }
-
-    async function create(attachment) {
-        return await localFetch("attachment.create", { attachment }, conf.apiVersion);
+    async function create(objectId, file) {
+        return await localFetch("workspace.uploadFile", {
+            workspaceId: conf.workspaceId,
+            objectId: objectId,
+            file: file
+        }, conf.apiVersion);
     }
 
     async function update(attachment) {
@@ -138,14 +96,6 @@ function AttachmentLocal(conf = {}) {
 
     async function destroy(attachment) {
         return await localFetch("attachment.destroy", { attachment }, conf.apiVersion);
-    }
-
-    async function uploadFile(objectId, file) {
-        return await localFetch("workspace.uploadFile", {
-            workspaceId: conf.workspaceId,
-            objectId: objectId,
-            file: file
-        }, conf.apiVersion);
     }
     
 } 
